ade49f2169fa5c7c21ee2ed0a25d6baaccee64bf
white <whitedeveloper0203@mail.ru>
2019-04-17 00:09:18 +1000

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,7 @@ export const environment = {
   username: 'hello42@cyntexa.com',
   password: 'Cyntexa@123WqcLKkd0BpSdG8fKqkAHzwbZ',
   // Local API URL
-  server_url: 'https://cann-demo.herokuapp.com'
+  server_url: 'https://devosha.herokuapp.com'
+  // My herokou URL 
+  // server_url: 'https://cann-demo.herokuapp.com'
 };
